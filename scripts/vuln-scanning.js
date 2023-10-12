e36638c8a3cf37b38549995dfc2539ecae1a47a7
Mijanur Rahman <onlineghor87@gmail.com>
2023-10-12 20:23:05 +0600

    updated

---
+++ scripts/vuln-scanning.js
@@ -0,0 +1,21 @@
+function googleSearch(type) {
+    var targetDomain = document.querySelector('.target').value;
+
+    if (!targetDomain) {
+        alert('Please enter a target domain.');
+        return;
+    }
+
+    var site = targetDomain;
+    switch (type) {
+        case 1:
+            var url = 'https://quttera.com/detailed_report/' + encodeURIComponent(site);
+            window.open(url, '_blank');
+            return;
+            
+        default:
+            alert('Invalid option.');
+            return;
+    }
+
+}
